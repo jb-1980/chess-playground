0eafe835f99c9b343470db2c40cd95195526a8e9
joseph <gilgenlabs@gmail.com>
2024-09-24 07:27:25 -0700

    ensure authentication for websocket

--- server-python/server/websocket/__init__.py
+++ server-python/server/websocket/__init__.py
@@ -1,39 +1,51 @@
 import json
 from pprint import pprint
 
+from flask import request
+from flask_jwt_extended import decode_token
 from flask_sock import Sock
+from jwt import InvalidSignatureError
 from pydantic import ValidationError
+
 from server.websocket.handle_error import handle_error
 from server.websocket.handle_get_game import handle_get_game
 from server.websocket.handle_join_game import handle_join_game
 from server.websocket.handle_move import handle_move
-from server.websocket.request_message_types import (RequestMessage,
-                                                    RequestMessageTypes)
+from server.websocket.request_message_types import RequestMessage, RequestMessageTypes
 
 sock = Sock()
 
 
 @sock.route("/")
 def websocket(ws):
+    token_bearer = request.headers.get("sec-websocket-protocol")
+    if not token_bearer:
+        handle_error("Missing token", ws)
+        return
+
+    try:
+        decode_token(token_bearer.split(" ")[1])
+    except InvalidSignatureError:
+        handle_error("Invalid token", ws)
+        return
+
     while True:
+        received_message = ws.receive()
+
         try:
-            request = ws.receive()
-            pprint({"request": request})
-        except Exception as e:
-            print("error: ", e)
-            return handle_error("Invalid request", ws)
-        try:
-            request_json = json.loads(request)
+            request_json = json.loads(received_message)
         except json.JSONDecodeError:
-            print(request)
-            return handle_error("Invalid JSON", ws)
+            print(received_message)
+            handle_error("Invalid JSON", ws)
+            return
+
         try:
-            data = RequestMessage(data=request_json)
+            message = RequestMessage(data=request_json).data
         except ValidationError as e:
             pprint(e)
-            return handle_error("Invalid payload", ws)
+            handle_error("Invalid payload", ws)
+            return
 
-        message = data.data
         match message.type:
             case RequestMessageTypes.JOIN_GAME:
                 handle_join_game(message, ws)
